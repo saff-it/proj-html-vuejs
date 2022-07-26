c46bd6dc5b2c6c421c3a7793fb96306054870b3d
saff-it <saff_it@ymail.com>
2022-07-26 16:13:30 +0200

    Header - finished, gradients to be implmented next

--- src/components/siteHeader.vue
+++ src/components/siteHeader.vue
@@ -26,18 +26,18 @@
                 <div class="col-5 ">
 
                     <div class="text-white">
-                        <h1>Business Planning</h1>
-                        <h1>Digital Technology</h1>
-                        <h1>Modern Solution</h1>
+                        <h1 class="fw-bolder">Business Planning</h1>
+                        <h1 class="fw-lighter">Digital Technology</h1>
+                        <h1 class="fw-bolder">Modern Solution</h1>
                     </div>
 
 
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, odit deserunt cum veniam
-                        quaerat quisquam eos, maiores unde optio esse nemo ipsum.</p>
+                        quaerat quisquam eos.</p>
 
                     <div class="ms_hero-btns">
-                        <button>Try it Now</button>
-                        <button>Explore More</button>
+                        <button class="ms_btn-left">Try it Now</button>
+                        <button class="ms_btn-right">Explore More</button>
                     </div>
                 </div>
 
@@ -139,12 +139,35 @@ nav.ms_top-header {
 
 // HERO
 
-.ms_hero-btns button{
-    @include buttonLayout;
-    
+.ms_hero{
+    padding-bottom: 75px;
+}
+
+.col-5 p{
+    color: $colorBg3;
+    width: 60%;
+    font-size: 0.7rem;
+    margin: 30px 0;
 
 }
 
+.col-7 img{
+    transform: scale(95%)
+}
+
+.ms_hero-btns button{
+    @include buttonLayout;
+    margin: 15px 15px 0 0;
+    color: white;
+}
+
+button.ms_btn-left{
+    background-color: $colorText9;
+}
+
+button.ms_btn-right{
+    background-color: $colorBg3;
+}
 
 
 </style>
