5afe7f910a785bc46087daa91aac00b839e31f28
saff-it <saff_it@ymail.com>
2022-07-26 12:21:11 +0200

    Vue CLI 3 main components set up and connection, bootstrap installation, styles importation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <siteHeader />
+    <siteMain />
+    <siteFooter />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import siteHeader from './components/siteHeader.vue'
+import siteMain from './components/siteMain.vue'
+import siteFooter from './components/siteFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    siteHeader,
+    siteMain,
+    siteFooter,
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap.scss";
+
+@import "./styles/general.scss";
+@import "./styles/variables.scss";
+
+
+
+
 </style>
